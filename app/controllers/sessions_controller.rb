08d34670333c0177eeaf74dd41580679fa04e860
”tomofumi03” <kuchibuefuite@icloud.com>
2021-05-04 09:27:49 +0900

    change login redirect_to

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email:params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to 'root_path'
+      redirect_to root_path
     else
       flash.now[:danger] = "認証できませんでした"
       render 'new'
