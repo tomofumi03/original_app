22c1c722572a0c9ef0931308f3788963bf2e9829
”tomofumi03” <kuchibuefuite@icloud.com>
2021-05-05 19:37:18 +0900

    add remember me

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,13 +21,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
     assert is_logged_in?
-    assert_redirected_to 'root_path'
+    assert_redirected_to root_url
     #follow_redirect!
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path, count: 0
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
