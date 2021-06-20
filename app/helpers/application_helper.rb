7faa6e4dd1c3ff0d094164845bceae72d85ab16d
”tomofumi03” <kuchibuefuite@icloud.com>
2021-06-20 17:10:28 +0900

    edit DM

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,21 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def room
+    binding.pry
+    @user = User.find(current_user.id)
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @adminuser = Entry.where(user_id: 1)
+    @currentUserEntry.each do |cu|
+      @adminuser.each do |ad|
+        if cu.room_id == ad.room_id then
+          @roomId = cu.room_id
+        end
+      end
+    end
+
+  end
+
+
 end
