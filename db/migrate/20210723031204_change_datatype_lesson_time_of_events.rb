6343383a429c4b80668c30eb8f53ca4de488363c
”tomofumi03” <kuchibuefuite@icloud.com>
2021-07-24 19:15:10 +0900

    change migration

--- db/migrate/20210723031204_change_datatype_lesson_time_of_events.rb
+++ db/migrate/20210723031204_change_datatype_lesson_time_of_events.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeLessonTimeOfEvents < ActiveRecord::Migration[5.1]
   def change
-    change_column :events, :lesson_time, :integer
+    change_column :events, :lesson_time, 'integer USING CAST(column_name AS integer)'
   end
 end
