f813fc18fa24bafe0699c6e625cd6e4b10ced4cf
”tomofumi03” <kuchibuefuite@icloud.com>
2021-07-27 22:14:14 +0900

    remove events cullum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210724130128) do
+ActiveRecord::Schema.define(version: 20210727131242) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 20210724130128) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "number"
-    t.integer "lesson_time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
