23dd6435ba45b2771a6df24141cbfb6629c71584
”tomofumi03” <kuchibuefuite@icloud.com>
2021-07-27 22:22:22 +0900

    add lesson_time collum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210727131242) do
+ActiveRecord::Schema.define(version: 20210727132134) do
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20210727131242) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "number"
+    t.integer "lesson_time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
