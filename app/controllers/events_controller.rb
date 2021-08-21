4cebf333dd4713efbe6ac2d615027356cd024170
”tomofumi03” <kuchibuefuite@icloud.com>
2021-08-21 20:04:27 +0900

    add edit an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,7 @@ class EventsController < ApplicationController
   before_action :logged_in_user
 
   def index
+    #binding.pry
     @today = Date.today
     from_date = Date.new(@today.year, @today.month, @today.beginning_of_month.day).beginning_of_week(:sunday)
     to_date = Date.new(@today.year, @today.month, @today.end_of_month.day).end_of_week(:sunday)
@@ -21,13 +22,12 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
     if @event.update_attributes(event_params)
-      redirect_to event_path
+      redirect_to event_path(@event.user_id)
       flash[:success] = "イベントが変更されました"
     else
       render 'edit'
     end
   end
-
 
   def create
     @event = Event.create(event_params)
@@ -36,6 +36,7 @@ class EventsController < ApplicationController
       redirect_to event_path(@event.user_id)
     else
       flash[:danger] = "イベントを追加できませんでした"
+      redirect_to event_path(@event.user_id)
     end
   end
 
@@ -43,9 +44,17 @@ class EventsController < ApplicationController
     #binding.pry
     @event = Event.new
     @events = Event.where(user_id: params[:id])
+
     @user = User.find(params[:id])
     @all_events = Event.all
+  end
 
+  def destroy
+    #binding.pry
+    @event = Event.find(params[:id])
+    @event.destroy
+    flash[:success] = "削除しました"
+    redirect_to event_path(@event.user_id)
   end
 
   private
