e5e114d5b70a8610c712a6e72aab8c2960892789
”tomofumi03” <kuchibuefuite@icloud.com>
2021-09-07 22:44:50 +0900

    delete - in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method       = :smtp
-───────
+  
   config.action_mailer.delicery_method       = :smtp
 
   config.action_mailer.default_url_options   = { host: 'original-piano-app.herokuapp.com' }
