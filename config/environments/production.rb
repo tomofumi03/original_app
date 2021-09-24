ff30c050acda04f870ae855b6e58497bc447b5c6
”tomofumi03” <kuchibuefuite@icloud.com>
2021-09-24 18:57:02 +0900

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
       authentication:       :plain,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
-      domain:               'herokuapp.com',
+      domain:               'em2813.heroku.com',
       enable_starttls_auto: true
   }
 end
