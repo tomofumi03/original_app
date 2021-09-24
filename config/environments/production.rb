64650ed565a2a699ef0f0e3b445c7717fef1cfce
”tomofumi03” <kuchibuefuite@icloud.com>
2021-09-24 18:27:05 +0900

    delete .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,17 +91,16 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.delivery_method       = :smtp
-
-  config.action_mailer.default_url_options   = { host: 'original-piano-app.herokuapp.com' }
+  host: 'original-piano-app.herokuapp.com'
+  config.action_mailer.default_url_options   = { host: host }
   ActionMailer::Base.smtp_settings           = {
       address:              'smtp.sendgrid.net',
       port:                 '587',
       authentication:       :plain,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
-      domain:               ENV['SENDGRID_DOMAIN'],
+      domain:               "herokuapp.com",
       enable_starttls_auto: true
   }
 end
