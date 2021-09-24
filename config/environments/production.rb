763ad7887879972331b2458b82efd3c14489795b
”tomofumi03” <kuchibuefuite@icloud.com>
2021-09-24 18:31:57 +0900

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,16 +91,17 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method       = :smtp
-  host: 'original-piano-app.herokuapp.com'
-  config.action_mailer.default_url_options   = { host: host }
+
+  config.action_mailer.default_url_options   = { host: 'original-piano-app.herokuapp.com' }
   ActionMailer::Base.smtp_settings           = {
       address:              'smtp.sendgrid.net',
       port:                 '587',
       authentication:       :plain,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
-      domain:               "herokuapp.com",
+      domain:               'herokuapp.com',
       enable_starttls_auto: true
   }
 end
