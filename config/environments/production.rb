2280b512bcb15c8e8aab5515e94cc9941d0ee9bd
”tomofumi03” <kuchibuefuite@icloud.com>
2021-09-26 15:27:29 +0900

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
       authentication:       :plain,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
-      domain:               'em2813.heroku.com',
+      domain:               'addons.heroku.com',
       enable_starttls_auto: true
   }
 end
