48e27b8f51451d44fc7d23ca606679f25ec8b943
”tomofumi03” <kuchibuefuite@icloud.com>
2021-09-27 21:43:13 +0900

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
       authentication:       :plain,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
-      domain:               'addons.heroku.com',
+      domain:               'original-piano-app.herokuapp.com',
       enable_starttls_auto: true
   }
 end
