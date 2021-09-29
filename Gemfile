9fea256e414b26811d4a27ba2693c1e85984d12c
”tomofumi03” <kuchibuefuite@icloud.com>
2021-09-29 21:27:16 +0900

    reset sendgrid

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'will_paginate',           '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
 gem 'simple_calendar', '~> 2.0'
 gem 'dotenv-rails'
+gem 'sendgrid-ruby'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,10 +99,9 @@ Rails.application.configure do
       address:              'smtp.sendgrid.net',
       port:                 '587',
       authentication:       :plain,
-      user_name:            ENV['SENDGRID_USERNAME'],
-      password:             ENV['SENDGRID_PASSWORD'],
-      api_key:              ENV['SENDGRID_API_KEY'],
-      domain:               'original-piano-app.herokuapp.com',
+      user_name:            'apikey',
+      password:             ENV['SENDGRID_APIKEY'],
+      domain:               'heroku.com',
       enable_starttls_auto: true
   }
 end
